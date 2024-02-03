c3cc0d03320ecadbcd2cfd280325fabee35b5037
laura galviz <galvizlaura69@gmail.com>
2024-02-03 14:32:14 -0500

    cambios

--- src/post/pages/AllUsers.js
+++ src/post/pages/AllUsers.js
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from "react";
-
 import Box from "@mui/material/Box";
 import Button from '@mui/material/Button';
 import { Grid, Modal, Pagination, ToggleButton, Typography } from "@mui/material";
-import CardUser from "../components/CardUser";
+import { CheckCircleOutlineRounded } from "@material-ui/icons";
 import getUsersAll from "../hooks/getUsersAll";
+import CardUser from "../components/CardUser";
 import FormCreateUser from "../components/FormCreateUser";
-import { styleModal } from "../styles/modal";
 import FormUpdateUser from "../components/FormUpdateUser";
 import FormDeleteUser from "../components/FormDeleteUser";
-
-import { CheckCircleOutlineRounded } from "@material-ui/icons";
+import { styleModal } from "../styles/modal";
 
 
 const AllUsers = ({ user }) => {
@@ -21,8 +19,8 @@ const AllUsers = ({ user }) => {
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(9);
   const [searchTerm, setSearchTerm] = useState('');
-  const [userSelected, setUserSelected]=useState(); 
-  const [areMyUsers, setAreMyUsers]=useState(false);
+  const [userSelected, setUserSelected] = useState();
+  const [areMyUsers, setAreMyUsers] = useState(false);
 
   const handleOpen = () => setOpen(true);
   const handleCloseDelete = () => {
@@ -45,26 +43,26 @@ const AllUsers = ({ user }) => {
   };
 
   const getList = async () => {
-    const {users, total} = await getUsersAll(page-1, 4, areMyUsers ); 
+    const { users, total } = await getUsersAll(page - 1, 4, areMyUsers);
     setTotalPages(total);
 
-    const fullNameUsers =  users.map((itemUser)=> {
+    const fullNameUsers = users.map((itemUser) => {
       itemUser.fullNameUser = `${itemUser.title}. ${itemUser.firstName} ${itemUser.lastName}`;
       return itemUser;
     });
 
-    const filteredUsers= fullNameUsers.filter(itemUser =>{
-        return  itemUser.fullNameUser.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
+    const filteredUsers = fullNameUsers.filter(itemUser => {
+      return itemUser.fullNameUser.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
     }
     );
     setListaPost(filteredUsers);
-    };
+  };
 
   useEffect(() => {
     getList();
     // setTotalPages(Math.ceil(response.data.total / 10)); // Calcula el número total de páginas
-    setTotalPages(totalPages/10); // Calcula el número total de páginas
-    
+    setTotalPages(totalPages / 10); // Calcula el número total de páginas
+
   }, [page, searchTerm, areMyUsers]);
 
   const handlePageChange = (event, newPage) => {
@@ -73,34 +71,34 @@ const AllUsers = ({ user }) => {
 
   return (
     <>
-          <input
-            type="text"
-            placeholder="Buscar..."
-            value={searchTerm}
-            onChange={handleSearchChange}
-          />
-          <Typography sx={{ fontWeight: 'bold' }} align="left" component="p">only my users</Typography>
+      <input
+        type="text"
+        placeholder="Buscar..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <Typography sx={{ fontWeight: 'bold' }} align="left" component="p">only my users</Typography>
 
-          <ToggleButton
-            value="check"
-            selected={areMyUsers}
-            onChange={() => {
-              setAreMyUsers(!areMyUsers);
-            }}
-          >
-            <CheckCircleOutlineRounded />
-          </ToggleButton>
-          {!areMyUsers && (
-            <Pagination
-            count={totalPages}
-            page={page}
-            onChange={handlePageChange}
-            variant="outlined"
-            shape="rounded"
-          /> 
-          )}
+      <ToggleButton
+        value="check"
+        selected={areMyUsers}
+        onChange={() => {
+          setAreMyUsers(!areMyUsers);
+        }}
+      >
+        <CheckCircleOutlineRounded />
+      </ToggleButton>
+      {!areMyUsers && (
+        <Pagination
+          count={totalPages}
+          page={page}
+          onChange={handlePageChange}
+          variant="outlined"
+          shape="rounded"
+        />
+      )}
 
-          <Button variant="contained" onClick={handleOpen}>CREAR NUEVO</Button>
+      <Button variant="contained" onClick={handleOpen}>CREAR NUEVO</Button>
 
       <Box m={2} p={2}>
         <Typography sx={{ fontWeight: 'bold' }} align="center" variant="h4" component="h4" mb={8} mt={4} color="blue">usuarios</Typography>
@@ -108,17 +106,17 @@ const AllUsers = ({ user }) => {
           {listaPost.map((user) => (
             <Grid item s={3} key={user.id}>
               <Box m={2} p={2}>
-                <CardUser 
-                  user={user} 
+                <CardUser
+                  user={user}
                   handleClose={handleCloseUpdate}
                   setOpen={setOpenModalUpdate}
                   setUserSelected={setUserSelected}
-                  />
+                />
               </Box>
             </Grid>
           ))}
-        </Grid> 
-          <Modal
+        </Grid>
+        <Modal
           open={open}
           onClose={handleClose}
           aria-labelledby="modal-modal-title"
@@ -130,21 +128,21 @@ const AllUsers = ({ user }) => {
           </Box>
         </Modal>
         {userSelected && (
-                <Modal
-                open={openModalUpdate}
-                onClose={handleCloseUpdate}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-              >
-                <Box sx={styleModal}>
-                  <Typography  variant="h6" component="h2">
-                  <FormUpdateUser 
-                    user={userSelected}
-                    handleClose={handleCloseUpdate} />
-                  </Typography>
-                  <Button onClick={handleCloseUpdate}>Cerrar</Button>
-                </Box>
-            </Modal>
+          <Modal
+            open={openModalUpdate}
+            onClose={handleCloseUpdate}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={styleModal}>
+              <Typography variant="h6" component="h2">
+                <FormUpdateUser
+                  user={userSelected}
+                  handleClose={handleCloseUpdate} />
+              </Typography>
+              <Button onClick={handleCloseUpdate}>Cerrar</Button>
+            </Box>
+          </Modal>
         )}
 
         <Modal
@@ -159,16 +157,16 @@ const AllUsers = ({ user }) => {
           </Box>
         </Modal>
 
-      </Box>  
+      </Box>
       {!areMyUsers && (
-            <Pagination
-            count={totalPages}
-            page={page}
-            onChange={handlePageChange}
-            variant="outlined"
-            shape="rounded"
-          /> 
-          )}       
+        <Pagination
+          count={totalPages}
+          page={page}
+          onChange={handlePageChange}
+          variant="outlined"
+          shape="rounded"
+        />
+      )}
     </>
   );
 };
